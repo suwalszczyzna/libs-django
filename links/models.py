e4159951ca867b0b8271407933aeaad75e2f3de6
Damian Suwala <seniorkawosz@gmail.com>
2021-09-19 15:23:44 +0200

    tag and link models added

--- links/models.py
+++ links/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from core.models import NanoIdModel, TimeStampModel
 
-# Create your models here.
+
+class Tag(NanoIdModel):
+    name = models.CharField(unique=True, max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Link(NanoIdModel, TimeStampModel):
+    title = models.CharField(max_length=200)
+    url = models.CharField(max_length=255)
+    faviconUrl = models.CharField(max_length=255, null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['created']
