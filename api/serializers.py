16a6aff1f93e1d7766060e2b4016539fd6c3da0e
Damian Suwala <seniorkawosz@gmail.com>
2021-09-19 23:21:21 +0200

    create tag when link is added

--- api/serializers.py
+++ api/serializers.py
@@ -5,12 +5,23 @@ from links.models import Tag, Link
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ('id', 'name')
+        fields = ['id', 'name']
 
 
 class LinkSerializer(serializers.ModelSerializer):
-    tags = TagSerializer(read_only=True, many=True)
+    tags = TagSerializer(many=True)
 
     class Meta:
         model = Link
-        fields = ('id', 'title', 'url', 'faviconUrl', 'tags')
+        fields = ['id', 'title', 'url', 'faviconUrl', 'tags']
+
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags') or []
+        link = Link.objects.create(**validated_data)
+        for item in tags_data:
+            name = item.get('name')
+            if name:
+                tag, _ = Tag.objects.get_or_create(name=name)
+                link.tags.add(tag)
+
+        return link
--- links/models.py
+++ links/models.py
@@ -5,7 +5,7 @@ from .managers import PublishedManager
 
 
 class Tag(NanoIdModel):
-    name = models.CharField(unique=True, max_length=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
