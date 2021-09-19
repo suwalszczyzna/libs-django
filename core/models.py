79c334f89e29c792b5c76fbdbd9031ec63c47481
Damian Suwala <seniorkawosz@gmail.com>
2021-09-19 15:16:53 +0200

    fix nanoid.generate.generate in migration

--- core/models.py
+++ core/models.py
@@ -3,8 +3,12 @@ from nanoid import generate
 from django.db import models
 
 
+def generate_nanoid():
+    return generate()
+
+
 class NanoIdModel(models.Model):
-    id = models.CharField(max_length=15, default=partial(generate, size=10))
+    id = models.CharField(primary_key=True, max_length=30, default=generate_nanoid)
 
     class Meta:
         abstract = True
