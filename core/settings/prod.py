e5fcb1fa88713610220bea8728008664becf04e6
Damian Suwala <damian.suwala@gmail.com>
2021-09-21 20:56:07 +0200

    heroku deploy

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,14 +1,27 @@
 from .base import *
+import django_heroku
 
 DEBUG = False
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '--',
-        'USER': '--',
-        'PASSWORD': '--',
-        'HOST': '--',
-        'PORT': '--',
+        'NAME': get_env_variable('POSTGRES_NAME'),
+        'USER': get_env_variable('POSTGRES_USER'),
+        'PASSWORD': get_env_variable('POSTGRES_PASSWORD'),
+        'HOST': get_env_variable('POSTGRES_HOST'),
+        'PORT': get_env_variable('POSTGRES_PORT'),
     }
 }
+
+django_heroku.settings(locals())
+ALLOWED_HOSTS = [
+    'libs-django.herokuapp.com',
+    'daemon-libs.netlify.app'
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'localhost:3000',
+    '127.0.0.1:3000',
+    'daemon-libs.netlify.app',
+]
